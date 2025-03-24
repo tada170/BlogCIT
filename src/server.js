0e1a72b935f6157e5e266f52af091dca7c9df327
Krasa Tadeas <tada.krasa@gmail.com>
2025-03-24 20:05:43 +0100

    init

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
@@ -21,16 +20,16 @@ const postSchema = new mongoose.Schema({
 
 const Post = mongoose.model('Post', postSchema);
 
-app.get('/api/posts', async (req, res) => {
+const getPosts = async (req, res) => {
   try {
     const posts = await Post.find();
     res.json(posts);
   } catch (error) {
     res.status(500).send('Chyba při získávání příspěvků');
   }
-});
+};
 
-app.post('/api/posts', async (req, res) => {
+const createPost = async (req, res) => {
   const { title, content } = req.body;
   const newPost = new Post({ title, content });
   try {
@@ -39,10 +38,10 @@ app.post('/api/posts', async (req, res) => {
   } catch (error) {
     res.status(500).send('Chyba při vytváření příspěvku');
   }
-});
+};
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'index.html'));
+  res.sendFile(path.join(__dirname, '../html/index.html'));
 });
 
 app.listen(PORT, () => {
