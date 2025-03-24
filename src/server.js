38996561911f8b652ee504cb8e6ed94d9d37d5ee
Krasa Tadeas <tada.krasa@gmail.com>
2025-03-24 19:59:26 +0100

    init

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
@@ -19,16 +21,16 @@ const postSchema = new mongoose.Schema({
 
 const Post = mongoose.model('Post', postSchema);
 
-const getPosts = async (req, res) => {
+app.get('/api/posts', async (req, res) => {
   try {
     const posts = await Post.find();
     res.json(posts);
   } catch (error) {
     res.status(500).send('Chyba při získávání příspěvků');
   }
-};
+});
 
-const createPost = async (req, res) => {
+app.post('/api/posts', async (req, res) => {
   const { title, content } = req.body;
   const newPost = new Post({ title, content });
   try {
@@ -37,10 +39,11 @@ const createPost = async (req, res) => {
   } catch (error) {
     res.status(500).send('Chyba při vytváření příspěvku');
   }
-};
+});
 
-app.get('/api/posts', getPosts);
-app.post('/api/posts', createPost);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
