ccb25588205fd33be887ace35693ada8401dc89c
Krasa Tadeas <tada.krasa@gmail.com>
2025-03-24 20:00:47 +0100

    init

--- src/server.js
+++ src/server.js
@@ -42,7 +42,7 @@ app.post('/api/posts', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'html', 'index.html'));
 });
 
 app.listen(PORT, () => {
