160bd0e0eae3deb62525df09cbe99a9b89839694
Krasa Tadeas <tada.krasa@gmail.com>
2025-03-24 20:29:08 +0100

    init

--- src/server.js
+++ src/server.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const cors = require('cors');  // Přidáme CORS pro umožnění přístupu z jiných domén
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-// Povolení CORS (pokud bude potřeba pro přístup z jiných domén)
 app.use(cors());
 
-// Statické soubory pro frontend
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-// Připojení k MongoDB
-mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/news_db', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.log('Error connecting to MongoDB:', err));
 
-// Mongoose schéma pro příspěvek
 const postSchema = new mongoose.Schema({
   title: String,
   content: String,
@@ -25,7 +21,6 @@ const postSchema = new mongoose.Schema({
 
 const Post = mongoose.model('Post', postSchema);
 
-// API pro načítání příspěvků
 app.get('/api/posts', async (req, res) => {
   try {
     const posts = await Post.find();
@@ -35,12 +30,10 @@ app.get('/api/posts', async (req, res) => {
   }
 });
 
-// Servírování HTML souboru
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../html', 'index.html'));  // Opravená cesta k HTML souboru
+  res.sendFile(path.join(__dirname, '../html', 'index.html'));
 });
 
-// Spuštění serveru
 app.listen(PORT, () => {
   console.log(`Server běží na http://localhost:${PORT}`);
 });
